998db82031429b41d84088533c84e143e8127ba3
Ashish Rathi <arathi@crusoeenergy.com>
2026-01-20 12:32:16 -0800

    fetch and apply node labels

--- k8s/vector-config-reloader/app/vector_config_reloader_app.py
+++ k8s/vector-config-reloader/app/vector_config_reloader_app.py
@@ -18,22 +18,6 @@ KUBE_STATE_METRICS_TRANSFORM_SOURCE = LiteralStr("""
 .tags.crusoe_resource = "cmk"
 """)
 NODE_METRICS_VECTOR_TRANSFORM_NAME = "enrich_node_metrics"
-NODE_METRICS_VECTOR_TRANSFORM_SOURCE = LiteralStr("""
-if exists(.tags.Hostname) {
-parts, _ = split(.tags.Hostname, ".")
-host_prefix = get(parts, [0]) ?? ""
-prefix_parts, _ = split(host_prefix, "-")
-nodepool_id_parts, _ = slice(prefix_parts, 0, length(prefix_parts) - 1)
-.tags.nodepool, _ = join(nodepool_id_parts, "-")
-} else if exists(.tags.host) {
-prefix_parts, _ = split(.tags.host, "-")
-nodepool_id_parts, _ = slice(prefix_parts, 0, length(prefix_parts) - 1)
-.tags.nodepool, _ = join(nodepool_id_parts, "-")
-}
-.tags.cluster_id = "${CRUSOE_CLUSTER_ID}"
-.tags.vm_id = "${VM_ID}"
-.tags.crusoe_resource = "vm"
-""")
 CUSTOM_METRICS_VECTOR_TRANSFORM_NAME = "enrich_custom_metrics"
 CUSTOM_METRICS_CONFIG_MAP_NAME = "crusoe-custom-metrics-config"
 CUSTOM_METRICS_CONFIG_MAP_KEY = "custom-metrics-config.yaml"
@@ -41,22 +25,6 @@ CUSTOM_METRICS_SCRAPE_ANNOTATION = "crusoe.ai/scrape"
 CUSTOM_METRICS_PORT_ANNOTATION = "crusoe.ai/port"
 CUSTOM_METRICS_PATH_ANNOTATION = "crusoe.ai/path"
 CUSTOM_METRICS_DEFAULT_SCRAPE_INTERVAL = 30
-CUSTOM_METRICS_VECTOR_TRANSFORM = {
-    "type": "remap",
-    "inputs": [],
-    "source": LiteralStr("""
-if exists(.tags.Hostname) {
-parts, _ = split(.tags.Hostname, ".")
-host_prefix = get(parts, [0]) ?? ""
-prefix_parts, _ = split(host_prefix, "-")
-nodepool_id_parts, _ = slice(prefix_parts, 0, length(prefix_parts) - 1)
-.tags.nodepool, _ = join(nodepool_id_parts, "-")
-}
-.tags.cluster_id = "${CRUSOE_CLUSTER_ID}"
-.tags.vm_id = "${VM_ID}"
-.tags.crusoe_resource = "custom_metrics"
-""")
-}
 
 SCRAPE_INTERVAL_MIN_THRESHOLD = 5
 SCRAPE_TIMEOUT_PERCENTAGE = 0.7
@@ -110,6 +78,33 @@ class VectorConfigReloader:
         # set proxy if enabled
         if self.sink_proxy_cfg.get("enabled"):
             self.custom_metrics_sink_config["proxy"] = self.sink_proxy_cfg
+
+        try:
+            node = self.k8s_api_client.read_node(self.node_name)
+            labels = node.metadata.labels
+            self.vm_id = labels.get("crusoe.ai/instance.id")
+            self.nodepool_id = labels.get("crusoe.ai/nodepool.id")
+            self.instance_type = labels.get("beta.kubernetes.io/instance-type")
+        except client.exceptions.ApiException as e:
+            print(f"Failed to fetch node labels: {e}")
+
+        self.node_metrics_vector_transform_source = LiteralStr(f"""
+.tags.nodepool = "{self.nodepool_id}"
+.tags.cluster_id = "${{CRUSOE_CLUSTER_ID}}"
+.tags.vm_id = "${{VM_ID}}"
+.tags.crusoe_resource = "vm"
+""")
+
+        self.custom_metrics_vector_transform = {
+            "type": "remap",
+            "inputs": [],
+            "source": LiteralStr(f"""
+.tags.nodepool = "{self.nodepool_id}"
+.tags.cluster_id = "${{CRUSOE_CLUSTER_ID}}"
+.tags.vm_id = "${{VM_ID}}"
+.tags.crusoe_resource = "custom_metrics"
+""")
+        }
 
         LOG.setLevel(reloader_cfg["log_level"])
 
@@ -169,7 +164,7 @@ class VectorConfigReloader:
         return config.get(deployment_name, {})
 
     @staticmethod
-    def build_deployment_transform_source(deployment_config: dict, endpoint_config: dict) -> str:
+    def build_deployment_transform_source(self, deployment_config: dict, endpoint_config: dict) -> str:
         vrl_lines = []
         allowlist = deployment_config.get("allowlist", [])
         droplist = deployment_config.get("droplist", [])
@@ -193,13 +188,7 @@ class VectorConfigReloader:
                 for key, value in label_entry.items():
                     vrl_lines.append(f'.tags.{key} = "{value}"')
 
-        vrl_lines.append('if exists(.tags.Hostname) {')
-        vrl_lines.append('parts, _ = split(.tags.Hostname, ".")')
-        vrl_lines.append('host_prefix = get(parts, [0]) ?? ""')
-        vrl_lines.append('prefix_parts, _ = split(host_prefix, "-")')
-        vrl_lines.append('nodepool_id_parts, _ = slice(prefix_parts, 0, length(prefix_parts) - 1)')
-        vrl_lines.append('.tags.nodepool, _ = join(nodepool_id_parts, "-")')
-        vrl_lines.append('}')
+        vrl_lines.append(f'.tags.nodepool = "{self.nodepool_id}"')
         vrl_lines.append('.tags.cluster_id = "${CRUSOE_CLUSTER_ID}"')
         vrl_lines.append('.tags.vm_id = "${VM_ID}"')
         vrl_lines.append('.tags.crusoe_resource = "custom_metrics"')
@@ -294,7 +283,7 @@ class VectorConfigReloader:
             }
 
             transform_name = f"{pod_name_sanitized}_transform"
-            transform_source = VectorConfigReloader.build_deployment_transform_source(deployment_config, endpoint)
+            transform_source = self.build_deployment_transform_source(deployment_config, endpoint)
             transforms[transform_name] = {
                 "type": "remap",
                 "inputs": [source_name],
@@ -377,7 +366,7 @@ class VectorConfigReloader:
 
         LOG.debug(f"Writing vector config {str(base_cfg)}")
         # always update the node metrics transform source to handle LiteralStr issue
-        base_cfg["transforms"][NODE_METRICS_VECTOR_TRANSFORM_NAME]["source"] = NODE_METRICS_VECTOR_TRANSFORM_SOURCE
+        base_cfg["transforms"][NODE_METRICS_VECTOR_TRANSFORM_NAME]["source"] = self.node_metrics_vector_transform_source
         YamlUtils.save_yaml(VECTOR_CONFIG_PATH, base_cfg)
         LOG.info(f"Vector config bootstrapped!")
 
@@ -434,7 +423,7 @@ class VectorConfigReloader:
 
         LOG.debug(f"Writing vector config: {str(current_vector_cfg)}")
         # always update the node metrics transform source to handle LiteralStr issue
-        current_vector_cfg["transforms"][NODE_METRICS_VECTOR_TRANSFORM_NAME]["source"] = NODE_METRICS_VECTOR_TRANSFORM_SOURCE
+        current_vector_cfg["transforms"][NODE_METRICS_VECTOR_TRANSFORM_NAME]["source"] = self.node_metrics_vector_transform_source
         YamlUtils.save_yaml(VECTOR_CONFIG_PATH, current_vector_cfg)
         LOG.info(f"Vector config reloaded!")
 
